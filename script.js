fc23784fd8dd7e2b6ba2dd046d2bc83afe691b6d
Apsar74 <115917243+Apsar74@users.noreply.github.com>
2023-08-30 10:56:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,8 @@ function renderGame(){
     cardsEl.textContent = "Cards: " 
     sumEl.textContent = "Sum :" + sum
     for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + ","
+        cardsEl.textContent += cards[i] + ""
+        cards.length = ","
     }
 }
 
